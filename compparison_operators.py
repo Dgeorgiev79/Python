38bc82f514faa8e7d4d27c1a5e9b62cbefe6ebfe
Dgeorgiev79 <50593975+Dgeorgiev79@users.noreply.github.com>
2022-12-04 16:18:31 +0200

    Create compparison_operators.py
    compare operators python script

---
+++ compparison_operators.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3.10
+
+x = 3 > 2
+print("Statement behaviour 3>2     ", x)
+print()
+
+y= 3>=2
+print("Statement behaviouri 3>=2 ",y)
+print()
+
+z=3<2
+print("Statement behaviour 3<2 ",z)
+print()
+
+g= 3==2
+print("Variable g 3==2 is ", g)
