c26211cff38bfa69ab85d7874f6991597a7c523e
Dgeorgiev79 <50593975+Dgeorgiev79@users.noreply.github.com>
2022-12-04 18:45:40 +0200

    Create kg_to_pounds.py

---
+++ kg_to_pounds.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3.10
+
+weight = input("Enter your weight in kg:  ")
+
+weight=float(weight)
+print("Your weight in kg is: ",weight)
+
+print("Your weight in pouns is:",weight * 2.20462)
