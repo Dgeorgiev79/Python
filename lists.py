ddd7ca91fbb919f1a755ec0242417037c7d8a05e
Dgeorgiev79 <50593975+Dgeorgiev79@users.noreply.github.com>
2022-12-16 20:04:02 +0200

    Create lists.py

---
+++ lists.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3.10
+
+names = ["Jimy","John","Jamesan"]
+print(names)
+
+names[0]="Dimi"
+print()
+print(names[0])
+print(names[1])
+print(names[2])
+
+print()
+print(names[0:2])
