149cfba8dcd0f971232dfe52065abb0cfde03278
Dgeorgiev79 <50593975+Dgeorgiev79@users.noreply.github.com>
2022-12-18 18:44:06 +0200

    Update lists.py
    exercise additional list funktions

--- lists.py
+++ lists.py
@@ -11,3 +11,19 @@ print(names[2])
 
 print()
 print(names[0:2])
+
+#empty row
+print()
+name=[1,2,3,4,5,6,7]
+name.append(8)
+#name.insert(2,33333)
+#name.insert(5,55555)
+print(name)
+print(len(name))
+
+#empty row
+print()
+name.remove(2)
+name.remove(5)
+print(name)
+print(len(name))
